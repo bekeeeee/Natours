aedd02d365ba31da9150d57d71ee1d6990764f9f
Mohamed Ragab <m.ragab9521@gmail.com>
2020-11-15 02:49:25 +0200

    edit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
-
+const compresion = require('compresion');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -67,7 +67,7 @@ app.use(
   })
 );
 
-
+app.use(compresion());
 
 
 // Test middleware
